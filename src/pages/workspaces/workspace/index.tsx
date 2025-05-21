4593501f95950b89cc0657a0dc291ad52b9f88f0
priya_patel <priyav.patel@tatvasoft.com>
2025-05-21 16:28:02 +0530

    changes in workspace details page

--- src/pages/workspaces/workspace/index.tsx
+++ src/pages/workspaces/workspace/index.tsx
@@ -298,7 +298,7 @@ const WorkspaceDetail: React.FC = () => {
           className="workspace-back"
           onClick={() => navigate(PRIVATE_ROUTE.WORKSPACES)}
         >
-          <ArrowLeftOutlined /> Back to workspace
+          <ArrowLeftOutlined style={{marginRight:'5px'}}/> Back to workspace
         </Text>
         </div>
         <Card className="workspace-header">
@@ -360,19 +360,33 @@ const WorkspaceDetail: React.FC = () => {
                         }}
                       >
                         <div className="info-card-content">
-                          <p>
-                            <strong>Created by:</strong>{" "}
+                          <div className="info-card-div">
+                            <div className="info-card-icon">
+                            <UserOutlined />
+                            </div>
+                            <div className="info-card-title-text">
+                            <Typography style={{color:'black'}}>Created by</Typography>{" "}
+                            <Typography style={{color:'gray'}}>
                             {selectedWorkspace.createdBy.first_name +
                               " " +
                               selectedWorkspace.createdBy.last_name}{" "}
                             ({selectedWorkspace.createdBy.email})
-                          </p>
-                          <p>
-                            <strong>Created at:</strong>{" "}
-                            {dayjs(
-                              selectedWorkspace.createdAt
-                            ).format("MMM DD,YYYY hh:mm A")}
-                          </p>
+                            </Typography>
+                              </div>
+                          </div>
+                          <div className="info-card-div">
+                            <div className="info-card-icon">
+                              <UserOutlined />
+                            </div>
+                            <div className="info-card-title-text">
+                              <Typography style={{color:'black'}}>Created at</Typography>{" "}
+                              <Typography style={{color:'gray'}}>
+                              {dayjs(
+                                selectedWorkspace.createdAt
+                              ).format("MMM DD,YYYY hh:mm A")}
+                              </Typography>
+                            </div>
+                          </div>
                         </div>
                       </Card>
                     </Col>
@@ -384,8 +398,10 @@ const WorkspaceDetail: React.FC = () => {
                           header: { borderTop: `3px solid #40a8ff7a`},
                         }}
                       >
-                        <div className="activity-content">
+                        <div className="activity-content-item">
+                          <ClockCircleOutlined style={{ fontSize: '25px' , color:'gray', margin:'15px 0px'}}/>
                           <Text type="secondary">No recent activity</Text>
+                          <Text type="secondary">Activities will appear here as you work</Text>
                         </div>
                       </Card>
                     </Col>
