f176cb743b48f417a40ba68f6e573840172de0fa
jeet1desai <jeetdesai995@gmail.com>
2025-06-04 11:31:15 +0530

    change format

--- src/config/firebase/helperFunction.ts
+++ src/config/firebase/helperFunction.ts
@@ -1,27 +1,14 @@
 import { FirebaseError } from "firebase/app";
-import {
-  GoogleAuthProvider,
-  FacebookAuthProvider,
-  OAuthProvider,
-  UserCredential,
-  AuthProvider,
-  signOut,
-  signInWithPopup,
-} from "firebase/auth";
+import { GoogleAuthProvider, FacebookAuthProvider, OAuthProvider, UserCredential, AuthProvider, signOut, signInWithPopup } from "firebase/auth";
 import { auth } from "./firebaseConfig";
 import { handleLoginResp } from "../../components/social";
 import { Dispatch } from "redux";
 
-export const getAccessToken = (
-  provider: AuthProvider,
-  result: UserCredential
-): string | null => {
+export const getAccessToken = (provider: AuthProvider, result: UserCredential): string | null => {
   if (provider instanceof GoogleAuthProvider) {
     return GoogleAuthProvider.credentialFromResult(result)?.accessToken ?? null;
   } else if (provider instanceof FacebookAuthProvider) {
-    return (
-      FacebookAuthProvider.credentialFromResult(result)?.accessToken ?? null
-    );
+    return FacebookAuthProvider.credentialFromResult(result)?.accessToken ?? null;
   } else if (provider instanceof OAuthProvider) {
     return OAuthProvider.credentialFromResult(result)?.accessToken ?? null;
   }
@@ -33,24 +20,16 @@ export const handleAuthError = (error: unknown, providerName: string) => {
   if (error instanceof FirebaseError) {
     switch (error.code) {
       case "auth/cancelled-popup-request":
-        console.warn(
-          `Popup was cancelled before completing Social login. Provider Name ==> ${providerName}`
-        );
+        console.warn(`Popup was cancelled before completing Social login. Provider Name ==> ${providerName}`);
         break;
       case "auth/popup-closed-by-user":
-        console.warn(
-          `Popup was closed before completing Social login. Provider Name ==> ${providerName}`
-        );
+        console.warn(`Popup was closed before completing Social login. Provider Name ==> ${providerName}`);
         break;
       case "auth/invalid-credential":
-        console.error(
-          `Invalid credential provided. Please check your configuration. Provider Name ==> ${providerName}`
-        );
+        console.error(`Invalid credential provided. Please check your configuration. Provider Name ==> ${providerName}`);
         break;
       default:
-        console.error(
-          `Firebase Error during login Provider Name ==> ${providerName}: ${error.message}`
-        );
+        console.error(`Firebase Error during login Provider Name ==> ${providerName}: ${error.message}`);
     }
   } else {
     // Handle unknown errors
@@ -66,7 +45,7 @@ export const handleSignIn = async (
 ) => {
   try {
     await handleSocialLogout();
-    const result:any = await signInWithPopup(auth, provider);
+    const result: any = await signInWithPopup(auth, provider);
     const token = await result.user.getIdToken();
     const screenName = result._tokenResponse?.screenName;
     handleLoginResp(screenName, token, dispatch, goTo);
@@ -79,8 +58,7 @@ export const handleSignIn = async (
   }
 };
 
-export const handleSocialLogout = async (
-) => {
+export const handleSocialLogout = async () => {
   try {
     await signOut(auth);
   } catch (e) {
