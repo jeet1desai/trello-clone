4b291f23aa85e1466a4ff031312c599c4e4f50e3
jeet1desai <jeetdesai995@gmail.com>
2025-06-09 13:48:46 +0530

    remove log

--- src/config/firebase/firebaseConfig.ts
+++ src/config/firebase/firebaseConfig.ts
@@ -42,7 +42,6 @@ export const generateToken = async () => {
     const fpn_token = await getToken(messaging, {
       vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY,
     });
-    console.log("fpn_token", fpn_token);
     try {
       await axiosInstance.put("/auth/save-device-token", {
         fpn_token,
